a56a59c1b50bf01ca00054837bcb184451313612
munisazohidova7 <zohidovamunisa49@gmail.com>
2025-03-11 00:41:08 +0500

    Add files via upload

---
+++ for/10_mart.js
@@ -0,0 +1,27 @@
+// let oquvchilar = ["Ali", "Madinabonu", "Javohir", "Ziyoda"];
+// oquvchilar.forEach(function (oquvci){
+//     console.log(oquvci)
+// })
+//
+// let narxlar = [25000, 18000, 32000, 50000];
+// narxlar.forEach(function (narx){
+//     console.log(narx)
+// })
+
+
+
+let mahsulotlar = ["olma", "olcha" , "gilos" , "tarvuz"]
+let narx = [30000 , 20000 , 50000 , 80000]
+let yuz =100; chegirma = 10;
+for (let i = 0; i < mahsulotlar.length; i++) {
+    let b = mahsulotlar[i] + ": " + narx[i]
+    if (narx[i]<50000) {
+        console.log(b)
+    }
+    if (narx[i] >= 50000) {
+        let foiz = narx[i] * chegirma / yuz
+        let chegirmaNarhi = narx[i] - foiz
+        console.log(b + " va bu maxsulot 10%li chegirmada, " + chegirmaNarhi+" so'mga harid qilishingiz mumkun ")
+    }
+
+}
